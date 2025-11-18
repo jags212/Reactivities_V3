596c8773e5d906846a1e3ab74d0a0749ddbb4928
Jagadish_Mohanty <mohanty.jagadish@cleanharbors.com>
2025-11-18 19:26:13 +0530

    End of section 5

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,43 @@
+using Application.Activities.Commands;
+using Application.Activities.Queries;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class ActivitiesController : BaseApiController
+{
+    [HttpGet]
+    public async Task<ActionResult<List<Activity>>> GetActivities()
+    {
+        return await Mediator.Send(new GetActivityList.Query());
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Activity>> GetActivity(string id)
+    {
+        return await Mediator.Send(new GetActivityDetails.Query { Id = id });
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<string>> CreateActivity([FromBody] Activity activity)
+    {
+        return await Mediator.Send(new CreateActivity.Command { Activity = activity });
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> EditActivity([FromBody] Activity activity)
+    {
+        await Mediator.Send(new EditActivity.Command { Activity = activity });
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteActivity(string id)
+    {
+        await Mediator.Send(new DeleteActivity.Command { Id = id });
+        return NoContent();
+    }
+
+}
